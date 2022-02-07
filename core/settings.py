fd67c12b8d7eec7f078575da634e1b598048ede9
gmadurov <gmadurov@gmail.com>
2022-02-07 15:15:49 +0100

    ff wat fixen this can now run

--- core/settings.py
+++ core/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.getcwd() == '/home/gmadurov/websitebullshits/DispuutStropdas':
     from environment.env import setENV, setURL
@@ -32,7 +29,10 @@ if os.getcwd() == '/home/gmadurov/websitebullshits/DispuutStropdas':
     DEBUG = True
 else:
     DEBUG = False
-# DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
