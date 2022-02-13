05440da6d91bc0dca85770a698c70c55a25e6f3b
gmadurov <gmadurov@gmail.com>
2022-02-13 14:53:33 +0100

    2

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ if os.getcwd() == '/home/gmadurov/websitebullshits/DispuutStropdas':
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = False
 else:
-    DEBUG = False
+    DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
