bcb6e7e25b306b17fca2e369a50de683fdaaf847
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-05-01 13:13:48 +0200

    all cache empty

--- users/models.py
+++ users/models.py
@@ -6,34 +6,43 @@ import uuid
 # Create your models here.
 
 
-
 class Lid(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     initials = models.CharField(max_length=2, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     educations = models.CharField(max_length=200, null=True, blank=True)
-    lichting  = models.CharField(max_length=200, null=True, blank=True)
-    vertical  = models.CharField(max_length=200, null=True, blank=True)
-    email = models.EmailField(max_length=500,  blank=True)
-    phone = models.CharField(max_length=500, null=True, blank=True, unique =True)
+    lichting = models.IntegerField()
+    vertical = models.IntegerField()
+    email = models.EmailField(max_length=500, blank=True)
+    phone = models.CharField(max_length=500, null=True, blank=True, unique=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     lid_image = models.ImageField(
-        upload_to='images/leden/',null=True, blank=True, default='images/leden/user-default.png')
+        upload_to="images/leden/",
+        null=True,
+        blank=True,
+        default="images/leden/user-default.png",
+    )
 
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.IntegerField(primary_key=True, unique= True)
+    # id = models.UUIDField(
+    #     default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    # )
 
     def __str__(self):
         return str(self.user.username)
+    # @property
+    # def id(self):
+    #     return int(self.lichting)*10 + int(self.vertical)
+
     @property
     def imageURL(self):
         try:
             url = self.lid_image.url
         except:
-            url = ''
+            url = ""
         return url
-    class Meta: 
-        ordering = ['lichting', 'vertical']
+
+    class Meta:
+        ordering = ["lichting", "vertical"]
